fa2b9c5171710d2b2ea14db289bd2658cc5130bb
Ver-Bena <good005003@naver.com>
2017-09-13 17:51:37 +0900

    complete markup main page and table markup in program page

---
+++ js/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+  // var scroll = $(document).scrollTop();
+  //
+  // if (0 <= scroll && scroll <= 700) { // scroll 위치가 100px일 때
+  //   // banner
+  //   $('.banner, .banner .intro .logo-white, .banner .intro > p, .banner .intro .get-ticket, .banner .intro .logo').addClass('active');
+  // } else {
+  //   // banner
+  //   $('.banner .intro .logo-white, .banner .intro > p, .banner .intro .get-ticket, .banner .intro .logo').removeClass('active');
+  // }
+  //
+  // if (100 <= scroll && scroll <= 1200) {
+  //   // article1
+  //   $('.section .article1 .title, .section .article1 h2, .section .article1 ul, .section .article1 .table-footer').addClass('active');
+  // } else {
+  //   // article1
+  //   $('.section .article1 .title, .section .article1 h2, .section .article1 ul, .section .article1 .table-footer').removeClass('active');
+  // }
+  //
+  // if (800 <= scroll && scroll <= 2000) {
+  //   // article2
+  //   $('.section .article2 h2:first-of-type, .section .article2 .praise, .section .article2 .desc, .section .article2 ul:first-of-type').addClass('active');
+  // } else {
+  //   // article2
+  //   $('.section .article2 h2:first-of-type, .section .article2 .praise, .section .article2 .desc, .section .article2 ul:first-of-type').removeClass('active');
+  // }
+  //
+  // if (1500 <= scroll && scroll <= 2395) {
+  //   // article2
+  //   $('.section .article2 h2:nth-of-type(2), .section .article2 .rock-on, .section .article2 ul:nth-of-type(2)').addClass('active');
+  // } else {
+  //   // article2
+  //   $('.section .article2 h2:nth-of-type(2), .section .article2 .rock-on, .section .article2 ul:nth-of-type(2)').removeClass('active');
+  // }
+  //
+  // if (2000 <= scroll && scroll <= 2395) {
+  //   // article3
+  //   $('.section .article3 .logo, .section .article3 .saturn').addClass('active');
+  // } else {
+  //   // article3
+  //   $('.section .article3 .logo, .section .article3 .saturn').removeClass('active');
+  // }
+  //
+  // $(document).scroll(function() {
+  //   scroll = $(document).scrollTop();
+  //   // console.log(scroll);
+  //
+  //   if (0 <= scroll && scroll <= 700) { // scroll 위치가 100px일 때
+  //     // banner
+  //     $('.banner, .banner .intro .logo-white, .banner .intro > p, .banner .intro .get-ticket, .banner .intro .logo').addClass('active');
+  //   } else {
+  //     // banner
+  //     $('.banner .intro .logo-white, .banner .intro > p, .banner .intro .get-ticket, .banner .intro .logo').removeClass('active');
+  //   }
+  //
+  //   if (100 <= scroll && scroll <= 1200) {
+  //     // article1
+  //     $('.section .article1 .title, .section .article1 h2, .section .article1 ul, .section .article1 .table-footer').addClass('active');
+  //   } else {
+  //     // article1
+  //     $('.section .article1 .title, .section .article1 h2, .section .article1 ul, .section .article1 .table-footer').removeClass('active');
+  //   }
+  //
+  //   if (800 <= scroll && scroll <= 2000) {
+  //     // article2
+  //     $('.section .article2 h2:first-of-type, .section .article2 .praise, .section .article2 .desc, .section .article2 ul:first-of-type').addClass('active');
+  //   } else {
+  //     // article2
+  //     $('.section .article2 h2:first-of-type, .section .article2 .praise, .section .article2 .desc, .section .article2 ul:first-of-type').removeClass('active');
+  //   }
+  //
+  //   if (1500 <= scroll && scroll <= 2395) {
+  //     // article2
+  //     $('.section .article2 h2:nth-of-type(2), .section .article2 .rock-on, .section .article2 ul:nth-of-type(2)').addClass('active');
+  //   } else {
+  //     // article2
+  //     $('.section .article2 h2:nth-of-type(2), .section .article2 .rock-on, .section .article2 ul:nth-of-type(2)').removeClass('active');
+  //   }
+  //
+  //   if (2000 <= scroll && scroll <= 2395) {
+  //     // article3
+  //     $('.section .article3 .logo, .section .article3 .saturn').addClass('active');
+  //   } else {
+  //     // article3
+  //     $('.section .article3 .logo, .section .article3 .saturn').removeClass('active');
+  //   }
+  })
+});
